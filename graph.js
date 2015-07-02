15a0b2617621dc5a8dc0ad116a2d3f1c01ed01ac
itzmjauz <itzmjauz@gmail.com>
2015-07-02 17:00:56 +0200

    add color parameter

--- graph.js
+++ graph.js
@@ -1,19 +1,25 @@
-var labels =  ["January", "February", "March", "April", "May", "June", "July"];
+var labels  = ["January", "February", "March", "April", "May", "June", "July"];
 var numbers = [[28, 48, 40, 19, 86, 27, 90], [65, 59, 80, 81, 56, 55, 40]];
+var colors  = [[220,220,220], [151,187,205]];
 
-function getData(labels, numbers) { // numbers being a 2D array of numbers ( row of rows )
+
+function rgba(r, g, b, a) {
+    return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+}
+
+function getData(labels, numbers, colors) { // numbers being a 2D array of numbers ( row of rows )
 
     var dataRow = [];
 
     for (var x = 0 ; x < numbers.length ; x++) {
         dataRow[x] = {
             label : "number row : " + x,
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
+            fillColor: rgba(colors[x][0], colors[x][1], colors[x][2], 0.2),
+            strokeColor: rgba(colors[x][0], colors[x][1], colors[x][2], 1),
+            pointColor: rgba(colors[x][0], colors[x][1], colors[x][2], 1),
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            pointHighlightStroke: rgba(colors[x][0], colors[x][1], colors[x][2], 1),
             data: numbers[x]
         }
     }
@@ -26,7 +32,7 @@ function getData(labels, numbers) { // numbers being a 2D array of numbers ( row
     return data;
 }
 
-var data = getData(labels, numbers);
+var data = getData(labels, numbers, colors);
 
 
 var ctx = document.getElementById("myChart").getContext("2d");
